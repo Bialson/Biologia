9669894fc7c9e217f1f3bf844433d796e0e80285
Bialson <bialson95@gmail.com>
2021-11-12 21:29:04 +0100

    Loader added

--- script/script.js
+++ script/script.js
@@ -5,20 +5,26 @@ function show(){
 
 var counter = 0;
 
+// function appendChild(){
+//     document.getElementById("progress").innerHTML += ".";
+// }
+
 function changeProgress(){
     counter++;
+    setTimeout(function(){document.getElementById("progress").innerHTML += "...";}, 500);
     switch (counter) {
         case 1:
-            document.getElementById("progress").innerHTML = "Loading resources...";
+            document.getElementById("progress").innerHTML = "Loading resources";
             break;
         case 2:
-            document.getElementById("progress").innerHTML = "Loading images...";
+            document.getElementById("progress").innerHTML = "Loading images";
             break;
         case 3:
-            document.getElementById("progress").innerHTML = "Patching...";
+            document.getElementById("progress").innerHTML = "Patching";
             break;
         case 4:
             show();
+            break;
         default:
             break;
     }
